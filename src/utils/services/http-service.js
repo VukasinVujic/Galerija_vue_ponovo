1dad43f39333240ad1190570703c5b1871b94dc3
VukasinVujic <vukasinvujic021@gmail.com>
2019-04-19 19:17:44 +0200

    Mistake with AllGalleries

--- src/utils/services/http-service.js
+++ src/utils/services/http-service.js
@@ -1,7 +1,10 @@
 import axios from 'axios'
 
 export default axios.create({
+
     baseURL: 'http://127.0.0.1:8000/api/',
+    // 'http://localhost/VIVIFY/napredni/Zavrsni/Laravel/Gallerija_laravel/public/api/',
+
     headers: {
         Accept: 'application/json'
     }
